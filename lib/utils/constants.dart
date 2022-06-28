a92bb89377189f5cac3a5466eacfdd4bd7bef4eb
Steve <stevelpak@gmail.com>
2022-06-28 08:24:14 +0500

    New Trackizer App

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color clActiveSubs = const Color(0xFFFFA699);
+Color clHighSubs = const Color(0xFFAD78FF);
+Color clLowesSubs = const Color(0xFF7DFFEE);
+Color bgUp = const Color(0xFF353542);
+Color bgDown = const Color(0xFF1C1C23);
+Color bgSubs = const Color(0xFF0E0E12);
